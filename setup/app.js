a3c7148f5137e9c6e62b73cd33d1df7652e3fe7c
Dan Possehl <danielbpossehl@gmail.com>
2021-12-08 11:19:49 -0700

    updated giveaway textContent

--- setup/app.js
+++ setup/app.js
@@ -29,8 +29,17 @@ const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
 let futureDate = new Date(2020, 4, 24, 11, 30, 0);
-// console.log(futureDate);
+console.log(futureDate);
 
 const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
 
-giveaway.textContent = `giveaway ends of ${year}`;
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()];
+console.log(weekday);
+
+giveaway.textContent = `giveaway ends of ${weekday} ${date} ${month} ${year} ${hours}:${minutes} `;
