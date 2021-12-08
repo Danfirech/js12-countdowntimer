9f0511ffbcd64d17cbfc7602a833b1ac6fd506d0
Dan Possehl <danielbpossehl@gmail.com>
2021-12-08 11:32:54 -0700

    created getReamianingTime function

--- setup/app.js
+++ setup/app.js
@@ -29,7 +29,6 @@ const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
 let futureDate = new Date(2020, 4, 24, 11, 30, 0);
-console.log(futureDate);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -40,6 +39,22 @@ month = months[month];
 const date = futureDate.getDate();
 
 const weekday = weekdays[futureDate.getDay()];
-console.log(weekday);
 
 giveaway.textContent = `giveaway ends of ${weekday} ${date} ${month} ${year} ${hours}:${minutes} `;
+
+// future time in ms
+
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  console.log(t);
+
+  // 1s = 1000ms
+  // 1m = 60s
+  // 1h = 60min
+  // 1d = 24hr
+}
+
+getRemainingTime();
