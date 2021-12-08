fc7dc2bf2c1f7baf18fd547411805d77b952bdad
Dan Possehl <danielbpossehl@gmail.com>
2021-12-08 11:51:03 -0700

    countdown in working order{

--- setup/app.js
+++ setup/app.js
@@ -28,7 +28,7 @@ const deadline = document.querySelector(".deadline");
 
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date(2020, 4, 24, 11, 30, 0);
+let futureDate = new Date(2022, 12, 24, 11, 30, 0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -55,6 +55,32 @@ function getRemainingTime() {
   // 1m = 60s
   // 1h = 60min
   // 1d = 24hr
+
+  // values in ms
+
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  //Calculate all
+  let days = t / oneDay;
+  days = Math.floor(days);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  // set values array
+  const values = [days, hours, minutes, seconds];
+
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  }
+
+  items.forEach(function (item, index) {
+    item.innerHTML = values[index];
+  });
 }
 
 getRemainingTime();
