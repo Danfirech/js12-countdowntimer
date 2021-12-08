75112b91c8dcb689172b57d7c130bf2e53f88a68
Dan Possehl <danielbpossehl@gmail.com>
2021-12-08 12:11:54 -0700

    finished countdown

--- setup/app.js
+++ setup/app.js
@@ -28,7 +28,7 @@ const deadline = document.querySelector(".deadline");
 
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date(2022, 12, 24, 11, 30, 0);
+let futureDate = new Date(2022, 11, 24, 11, 30, 0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -81,6 +81,10 @@ function getRemainingTime() {
   items.forEach(function (item, index) {
     item.innerHTML = values[index];
   });
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired"> Sorry, this giveaway has expired"</h4>`;
+  }
 }
 
 //countdown
